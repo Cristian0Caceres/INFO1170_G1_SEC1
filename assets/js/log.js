f4b1de6ad30545e6444c73a3cf4456f713bd8b20
chelo132 <mmatamala2022@alu.uct.cl>
2024-09-20 22:48:25 -0300

    inicio de sesion
    inicio de sesión con distinción si son una cuenta admin o una cuenta normal

--- assets/js/log.js
+++ assets/js/log.js
@@ -1,9 +1,16 @@
-
 function mostrarAlerta() {
     const urlParams = new URLSearchParams(window.location.search);
     const registro = urlParams.get('registro');
     if (registro === 'exitoso') {
         alert('Usuario registrado exitosamente');
+    }
+
+    // Obtener el valor del parámetro 'error'
+    const error = urlParams.get('error');
+
+    // Verificar si el error es de credenciales inválidas
+    if (error === 'credenciales_invalidas') {
+        alert('Las credenciales que ingresaste son incorrectas. Por favor, verifica tu correo y contraseña.');
     }
 }
 
