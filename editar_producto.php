38243a3c7fcd9d3b2dde9b30b8896894dedb2df5
NotLSane <renato.carrasco2024@alu.uct.cl>
2024-09-22 18:44:45 -0300

    .

--- editar_producto.php
+++ editar_producto.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; 
 $username = "root"; 
 $password = ""; 
-$dbname = "bd"; 
+$dbname = "bd";  
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
