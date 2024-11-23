e34361ff25196b6744e41aba51e5a27942ef63cb
RCarrascO <renato.carrasco2024@alu.uct.cl>
2024-11-22 21:59:42 -0300

    UPDATE | Promocion + Webscraping + CSS Login
    Se completa al 100% el uso de bootstrap en Login, se soluciona un bug en Promocion (No habian imagenes de header ni footer) y se confirma el uso de Webscraping en Promo y Admin

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'bd'
+    database: 'bd_pruebaws'
 });
 
 // Conectar a la base de datos
